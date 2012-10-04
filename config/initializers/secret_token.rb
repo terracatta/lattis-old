b781e11a566e3a709d19ed2cfbd403a665755b9a
Jason Meller <jason.meller@gmail.com>
2012-10-04 17:57:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lattis::Application.config.secret_token = 'e6b7bad7f57f9862a03b3a8ace02d05f28e1dac44bf379400fd8a5be9f1c14cd7fd61bc39cb5dbe95d4a084024be23bc497293552dc5ce5d419d4f41e878ceda'
