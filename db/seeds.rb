51d9d98cf3d005045077815553d511c7bd7e01e4
Jason Meller <jason.meller@gmail.com>
2012-10-05 09:51:14 -0400

    add asset compilation, delayed jobs, db migration and seed support to cap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 
-## Create Default Administrator Account
-admin = User.create({
+#
+# Create Default Administrator Account
+#
+admin = User.find_or_create_by_email({
   email: 'admin.user@lattis.app',
   first_name: 'admin',
   last_name: 'user',
